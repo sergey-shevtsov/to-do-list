5fbd23af3b1dd726dc3dd9d36bbfcd85772aa8c9
Shevtsov_Sergey <sh59-perm@yandex.ru>
2021-11-20 22:19:51 +0300

    Убрал лишний класс

--- app/src/main/java/com/sshevtsov/todolist/ui/main/Data.kt
+++ app/src/main/java/com/sshevtsov/todolist/ui/main/Data.kt
@@ -3,12 +3,10 @@ package com.sshevtsov.todolist.ui.main
 data class Data(
     var viewType: Int,
     val headerText: String? = null,
-    val noteItem: NoteItem? = null,
-    val noteItemSpannable: NoteItemSpannable? = null
+    val noteItem: NoteItem? = null
 ) {
     companion object {
         const val TYPE_HEADER = 0
         const val TYPE_NOTE = 1
-        const val TYPE_NOTE_SPANNABLE = 2
     }
 }
